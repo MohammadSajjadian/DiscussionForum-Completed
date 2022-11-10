38347abbedb2fe2c6b4e0e08685c4fdc17dfeb10
Mohammad sajjadian <2222h.com@gmail.com>
2022-11-10 16:49:07 +0330

    Add project files.

---
+++ Discussion Forum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Discussion_Forum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
