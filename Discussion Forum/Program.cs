57ccce62e65f25f16dac7f5baf6ff89309639fea
Mohammad sajjadian <2222h.com@gmail.com>
2022-11-10 17:33:59 +0330

    Create Layering and Identity for project.

--- Discussion Forum/Program.cs
+++ Discussion Forum/Program.cs
@@ -1,4 +1,15 @@
+using Discussion_Forum.Areas.Identity.Data;
+using Discussion_Forum.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DBforumConnection") ?? throw new InvalidOperationException("Connection string 'DBforumConnection' not found.");
+
+builder.Services.AddDbContext<DBforum>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<DBforum>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication(); ;
 
 app.UseAuthorization();
 
